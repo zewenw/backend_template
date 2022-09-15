4b211563f85f50f6bc075ef37b06e66f28113a70
hengshui <96031964+hengshui@users.noreply.github.com>
2022-09-15 13:52:20 +0800

    中间件服务启动脚本

---
+++ middle_service.bat
@@ -0,0 +1,3 @@
+start cmd /k "cd C:\work\tool\nacos\nacos-server\nacos\bin && startup.cmd -m standalone"
+start cmd /k "cd C:\work\tool\redis\redis-sever\redis-2.8.13 && .\redis-server.exe"
+start cmd /k "cd C:\work\tool\zookeeper\apache-zookeeper-3.7.1-bin\bin && .\zkServer.cmd"
