2af69e8a24747aba8b8b4289058c1363babe54ce
hengshui <96031964+hengshui@users.noreply.github.com>
2022-09-15 14:41:13 +0800

    集成zipkin+sleuth

--- middle_service.bat
+++ middle_service.bat
@@ -1,3 +1,4 @@
 start cmd /k "cd C:\work\tool\nacos\nacos-server\nacos\bin && startup.cmd -m standalone"
 start cmd /k "cd C:\work\tool\redis\redis-sever\redis-2.8.13 && .\redis-server.exe"
 start cmd /k "cd C:\work\tool\zookeeper\apache-zookeeper-3.7.1-bin\bin && .\zkServer.cmd"
+start cmd /k "cd C:\work\tool\zipkin\source &&  java -jar .\zipkin-server-2.23.18-exec.jar"
